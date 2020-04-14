ca94a89fe36ab510b7058a7bd869845c3796d304
SunliangzeSmile <sunliangzesmile@gmail.com>
2020-04-14 16:33:05 +0800

    更新插件，添加插件案例

--- licenseplate.js
+++ licenseplate.js
@@ -1,132 +1,146 @@
 var provinces = new Array('京', '沪', '浙', '苏', '粤', '鲁', '晋', '冀', '豫', '川', '渝',
-		'辽', '吉', '黑', '皖', '鄂', '津', '贵', '云', '桂', '琼', '青', '新', '藏', '蒙', '宁',
-		'甘', '陕', '闽', '赣', '湘');
+    '辽', '吉', '黑', '皖', '鄂', '津', '贵', '云', '桂', '琼', '青', '新', '藏', '蒙', '宁',
+    '甘', '陕', '闽', '赣', '湘');
 var keyNums = new Array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Q',
-		'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H',
-		'J', 'K', 'L', 'OK', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Del');
+    'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H',
+    'J', 'K', 'L', 'OK', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Del');
 var next = 0;
 
+/**
+ * 显示省份键盘
+ */
 function showProvince() {
-	$(".keybord_panel").empty();
-	var keyUl=document.createElement("ul");
-	$(keyUl).addClass("clearfix ul_pro");
-	$.each(provinces,function(i,e) {
-		$(keyUl).append("<li><span onclick='chooseProvince(this);'>" +e+"</span></li>");
-	});
-	$(keyUl).append("<li class='li_close' onclick='closePro();'><span>关闭</span></li>");
-	$(keyUl).append("<li class='li_clean' onclick='cleanPro();'><span>清空</span></li>");
-	$(".keybord_panel").append(keyUl);
+    $(".keybord").empty();
+    var keyUl = document.createElement("ul");
+    $(keyUl).addClass("clearfix provinces_keybord");
+    $.each(provinces, function(i, e) {
+        $(keyUl).append("<li><span onclick='chooseProvince(this);'>" + e + "</span></li>");
+    });
+    $(keyUl).append("<li class='key_close' onclick='closePro();'><span>关闭</span></li>");
+    $(keyUl).append("<li class='key_clean' onclick='cleanPro();'><span>清空</span></li>");
+    $(".keybord").append(keyUl);
 }
 
+/**
+ * 显示数字、字母键盘
+ * @param {*} index 
+ */
 function showKeybord(index) {
-	console.log(index);
-	$(".keybord_panel").empty();
-	var keyUl=document.createElement("ul");
-	$(keyUl).addClass("clearfix ul_keybord");
-	$.each(keyNums,function(i,e) {
-		if (index===1 && i<=9){
-
-		} else {
-			$(keyUl).append("<li class='ikey ikey"+i+"  "+
-					(i > 9 ? 'li_zm' : 'li_num') + ' ' + (i > 28 ? 'li_w' : '')
-					+"'><span onclick='choosekey(this,"+i+");'>" +e+"</span></li>");
-		}
-	});
-	$(".keybord_panel").append(keyUl);
-	$(".car_license").val($('.ul_input').text());
+    $(".keybord").empty();
+    var keyUl = document.createElement("ul");
+    $(keyUl).addClass("clearfix num_keybord");
+    $.each(keyNums, function(i, e) {
+        if (!(index === 0 && i <= 9)) {
+            $(keyUl).append("<li class='ikey ikey" + i + "  " +
+                (i > 9 ? 'key_zm' : 'key_num') + ' ' + (i > 28 ? 'key_w' : '') +
+                "'><span onclick='choosekey(this," + i + ");'>" + e + "</span></li>");
+        }
+    });
+    $(".keybord").append(keyUl);
+    $(".keyboard_input").val($('.key_input').text());
 }
 
+/**
+ * 选择省份
+ * @param {*} obj 
+ */
 function chooseProvince(obj) {
-	$('.input_pro span').text($(obj).text());
-	$('.input_pro').addClass('hasPro');
-	$('.input_pp').find('span').text('');
-	$('.ppHas').removeClass('ppHas');
-	next = 0;
-	showKeybord(next+1);
+    $('.province span').text($(obj).text());
+    $('.province').addClass('has_province');
+    $('.keynums').find('span').text('');
+    $('.has_keynums').removeClass('has_keynums');
+    next = 0;
+    showKeybord(0);
 }
 
-function choosekey(obj, jj) {
-	if (jj == 29) {
-		$('.keybord_panel').hide();
-	} else if (jj == 37) {
-		if ($('.ppHas').length == 0) {
-			$('.hasPro').find('span').text('');
-			$('.hasPro').removeClass('hasPro');
-			showProvince();
-			next = 0;
-		}
-		$('.ppHas:last').find('span').text('');
-		$('.ppHas:last').removeClass('ppHas');
-		next = next - 1;
-		if (next < 1) {
-			next = 0;
-		}
-	} else {
-		if (next > 5) {
-			return;
-		}
-		for (var i = 0; i < $(".input_pp").length; i++) {
-			if (next == 0 & jj < 10 &
-					$('.input_pp:eq(' + next + ')').hasClass('input_zim')) {
-				layer.msg('车牌第二位为字母');
-				return;
-			}
-			$('.input_pp:eq(' + next + ')').find('span').text($(obj).text());
-			$('.input_pp:eq(' + next + ')').addClass('ppHas');
-			next = next + 1;
-			if (next > 5) {
-				next = 6;
-			}
-			showKeybord(next+1);
-			$(".car_license").val($('.ul_input').text());
-			return;
-		}
-	}
+/**
+ * 选择数字、字母
+ * @param {*} obj 
+ * @param {*} index 
+ */
+function choosekey(obj, index) {
+    if (index == 29) {
+        $('.keybord').hide();
+    } else if (index == 37) {
+        if ($('.has_keynums').length == 0) {
+            $('.has_province').find('span').text('');
+            $('.has_province').removeClass('has_province');
+            showProvince();
+            next = 0;
+        } else {
+            $('.has_keynums:last').find('span').text('');
+            $('.has_keynums:last').removeClass('has_keynums');
+            next = next - 1;
+        }
+    } else {
+        $('.keynums:eq(' + next + ')').find('span').text($(obj).text());
+        $('.keynums:eq(' + next + ')').addClass("has_keynums");
+        next = next + 1;
+        next = next > 5 ? 5 : next;
+    }
+    $(".keyboard_input").val($('.key_input').text());
+    if (next === 0 || index == 29) {
+        return;
+    }
+    showKeybord(next);
 }
 
+/**
+ * 关闭
+ */
 function closePro() {
-	$('.keybord_panel').hide();
+    $('.keybord').hide();
+}
+/**
+ * 清空
+ */
+function cleanPro() {
+    $('.key_input').find('span').text('');
+    $('.has_province').removeClass('has_province');
+    $('.has_keynums').removeClass('has_keynums');
+    next = 0;
 }
 
-function cleanPro() {
-	$('.ul_input').find('span').text('');
-	$('.hasPro').removeClass('hasPro');
-	$('.ppHas').removeClass('ppHas');
-	next = 0;
-}
-function renderCarLicense(elem, chooseCallback) {
-	var carDiv=document.createElement('div');
-	$(carDiv).addClass("car_input");
-	$(elem).after(carDiv);
-	$(elem).addClass("car_license");
-	var ul = document.createElement('ul');
-	$(ul).addClass('clearfix ul_input');
-	$(carDiv).append("<div class='car_checkbox'><input type='checkbox' /><span>新能源</span></div>");
-	for (var index = 0; index < 6; index++) {
-		var li = document.createElement('li');
-		if (index === 0) {
-			$(li).addClass('input_pro hasPro');
-		} else if (index === 1) {
-			$(li).addClass('input_pp input_zim ppHas');
-		} else {
-			$(li).addClass('input_pp ppHas');
-		}
-		$(li).attr("index",index);
-		$(li).append('<span></span>');
-		$(ul).append(li);
-	}
-	$(carDiv).append(ul);
-	$(carDiv).append('<div  class=\'keybord_panel\'></div>');
-	$('.input_pro').click(function() {
-		$('.keybord_panel').show();
-		showProvince();
-	});
-	$('.input_pp').click(function() {
-		if ($('.input_pro').hasClass('hasPro')) {
-			$('.keybord_panel').show();
-			showKeybord($(this).attr("index"));
-		} else {
-			$('.input_pro').click();
-		}
-	});
+/**
+ * 渲染键盘
+ * @param {*} elem 
+ * @param {*} chooseCallback 
+ */
+function renderCarKeyBoard(elem, chooseCallback) {
+    var carDiv = document.createElement('div');
+    $(carDiv).addClass("car_input");
+    $(elem).after(carDiv);
+    $(elem).addClass("keyboard_input");
+    var ul = document.createElement('ul');
+    $(ul).addClass('clearfix key_input');
+    $(carDiv).append("<div class='keyboard_checkbox'><input type='checkbox' /><span>新能源</span></div>");
+    for (var index = 0; index < 6; index++) {
+        var li = document.createElement('li');
+        if (index === 0) {
+            $(li).addClass('province has_province');
+        } else if (index === 1) {
+            $(li).addClass('keynums key_zim has_keynums');
+        } else {
+            $(li).addClass('keynums has_keynums');
+        }
+        $(li).attr("index", index);
+        $(li).append("<span></span>");
+        $(ul).append(li);
+    }
+    $(carDiv).append(ul);
+    $(carDiv).append("<div class='keybord'></div>");
+    $('.province').click(function() {
+        $('.keybord').show();
+        showProvince();
+    });
+    $('.keynums').click(function() {
+        $('.keybord').show();
+        console.log($(this).attr("index"));
+        if ($('.province').hasClass('has_province')) {
+            $('.keybord').show();
+            showKeybord(0);
+        } else {
+            $('.province').click();
+        }
+    });
 }
